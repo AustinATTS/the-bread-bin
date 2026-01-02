19179cd965253851450ac484364177d515a42c6f
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-02 22:07:17 +0000

    Added the logic for saving a loaf

--- core/src/LoafFile.cpp
+++ core/src/LoafFile.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <breadbin/LoafFile.hpp>
 #include <yaml-cpp/yaml.h>
 #include <sstream>
@@ -7,5 +8,30 @@ namespace breadbin::core {
         std::stringstream ss;
         ss << "App: " << app_name << " (" << args.size() << " arguments) ";
         return ss.str();
+    }
+
+    bool LoafFile::save_to_file(const std::filesystem::path& path) const {
+        YAML::Emitter out;
+
+        out << YAML::BeginMap;
+        out << YAML::Key << "app_name" << YAML::Value << app_name;
+        out << YAML::Key << "args" << YAML::Value << YAML::BeginSeq;
+        for (const auto& arg : args) {
+            out << arg;
+        }
+        out << YAML::EndSeq;
+        out << YAML::EndMap;
+
+        std::ofstream fout(path);
+        if (!fout.is_open()) {
+            return false;
+        }
+
+        fout << out.c_str();
+        return true;
+    }
+
+    bool LoafFile::load_from_file(const std::filesystem::path& path) {
+
     }
 } // namespace breadbin::core
