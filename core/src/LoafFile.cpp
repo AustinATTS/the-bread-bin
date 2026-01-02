4af91ee41a441aed7a00e42daffacbb812e7c367
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-02 08:35:55 +0000

    Added the generic function to print out the app name and number of arguments with it

--- core/src/LoafFile.cpp
+++ core/src/LoafFile.cpp
@@ -1,3 +1,11 @@
-//
-// Created by austin on 02/01/2026.
-//
+#include <breadbin/LoafFile.hpp>
+#include <yaml-cpp/yaml.h>
+#include <sstream>
+
+namespace breadbin::core {
+    std::string LoafFile::get_summary() const {
+        std::stringstream ss;
+        ss << "App: " << app_name << " (" << args.size() << " arguments) ";
+        return ss.str();
+    }
+} // namespace breadbin::core
