c752a7dbd38762c13e68e5332cd531e389b0a967
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-02 10:20:17 +0000

    Added GUI code

--- apps/gui-editor/main_gui.cpp
+++ apps/gui-editor/main_gui.cpp
@@ -1,16 +1,89 @@
-#include <iostream>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <breadbin/LoafFile.hpp>
+#include <cstdio>
 
 int main() {
-    // Create a LOAF object using the defined struct
-    breadbin::core::LoafFile my_app;
-    my_app.app_name = "The Bread Bin Editor";
-    my_app.args = {"--verbose", "--no-save"};
+    if (!glfwInit()) {
+        return -1;
+    }
 
-    std::cout << "--- Breadbin GUI Starting ---" << std::endl;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Call the function defined in out Core library
-    std::cout << "Status: " << my_app.get_summary() << std::endl;
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "The Bread Bin - Editor", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    breadbin::core::LoafFile current_loaf;
+    current_loaf.app_name = "The Bread Bin - Editor";
+
+    static char buf[128] = {};
+    static bool initialized = false;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (!initialized) {
+            std::snprintf(buf, sizeof(buf), "%s", current_loaf.app_name.c_str());
+            initialized = true;
+        }
+
+        ImGui::Begin("Bread Bin Editor");
+
+        if (ImGui::InputText("Application Name", buf, sizeof(buf))) {
+            current_loaf.app_name = buf;
+        }
+
+        if (ImGui::Button("Save Loaf File")) {
+        }
+
+        ImGui::End();
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
