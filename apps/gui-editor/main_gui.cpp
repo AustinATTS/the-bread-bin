cae47c257978acfde77d782c49dc04623a6f13fc
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-02 22:27:16 +0000

    Added arguments

--- apps/gui-editor/main_gui.cpp
+++ apps/gui-editor/main_gui.cpp
@@ -42,7 +42,7 @@ int main ( ) {
     breadbin::core::LoafFile current_loaf;
     current_loaf.app_name = "The Bread Bin - Editor";
 
-    static char buf[128] = {};
+    static char name_buf[128] = {0};
     static bool initialized = false;
 
     while (! glfwWindowShouldClose(window)) {
@@ -53,18 +53,35 @@ int main ( ) {
         ImGui::NewFrame();
 
         if (! initialized) {
-            std::snprintf(buf, sizeof(buf), "%s", current_loaf.app_name.c_str());
+            std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.app_name.c_str());
             initialized = true;
         }
 
         ImGui::Begin("Bread Bin Editor");
 
-        if (ImGui::InputText("Application Name", buf, sizeof(buf))) {
-            current_loaf.app_name = buf;
+        if (ImGui::InputText("Application Name", name_buf, sizeof(name_buf))) {
+            current_loaf.app_name = name_buf;
+        }
+
+        if (ImGui::Button("+ Add Arg")) {
+            current_loaf.args.push_back("new_argument");
+        }
+
+        for (size_t i = 0; i < current_loaf.args.size(); i++) {
+            char arg_buf[128];
+            std::snprintf(arg_buf, sizeof(arg_buf), "%s", current_loaf.args[i].c_str());
+            if (ImGui::InputText(("##arg" + std::to_string(i)).c_str(), arg_buf, sizeof(arg_buf))) {
+                current_loaf.args[i] = arg_buf;
+            }
+            ImGui::SameLine();
+            if (ImGui::Button(("Remove##" + std::to_string(i)).c_str())) {
+                current_loaf.args.erase(current_loaf.args.begin() + i);
+            }
         }
 
         if (ImGui::Button("Save Loaf File")) {
-            if (current_loaf.save_to_file("config.loaf")) {
+            std::filesystem::path save_path = "settings.loaf";
+            if (current_loaf.save_to_file(save_path)) {
                 std::cout << "Successfully saved config.loaf" << std::endl;
             } else {
                 std::cout << "Failed to save config.loaf" << std::endl;
