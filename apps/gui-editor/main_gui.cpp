808ad4dc140959f851f816d4b3b642e7f34de45d
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-02 08:36:36 +0000

    Currently just a cli, adding an app manually and displaying it

--- apps/gui-editor/main_gui.cpp
+++ apps/gui-editor/main_gui.cpp
@@ -1,3 +1,17 @@
-//
-// Created by austin on 02/01/2026.
-//
+#include <iostream>
+#include <breadbin/LoafFile.hpp>
+
+int main()
+{
+    // Create a LOAF object using the defined struct
+    breadbin::core::LoafFile my_app;
+    my_app.app_name = "The Bread Bin Editor";
+    my_app.args = {"--verbose", "--no-save"};
+
+    std::cout << "--- Breadbin GUI Starting ---" << std::endl;
+
+    // Call the function defined in out Core library
+    std::cout << "Status: " << my_app.get_summary() << std::endl;
+
+    return 0;
+}
