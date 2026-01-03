b0a75a35e7721013888d72550fd9702fb65af76d
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-01-03 11:53:44 +0000

    Finally working docking and loading and file view

--- apps/gui-editor/main_gui.cpp
+++ apps/gui-editor/main_gui.cpp
@@ -3,67 +3,68 @@
 #include <imgui_impl_opengl3.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-#include <breadbin/LoafFile.hpp>
 #include <ThemeLoader.hpp>
+#include <LoafBrowser.hpp>
+#include <breadbin/LoafFile.hpp>
 #include <nfd.hpp>
-
 #include <cstdio>
-#include <iostream>
-#include <optional>
+#include <cstdlib>
 #include <filesystem>
+#include <optional>
+#include <map>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
-#include <map>
 
-#include <LoafBrowser.hpp>
+static bool show_editor = true;
+static bool show_browser = true;
+static bool loaf_dirty = false;
 
-std::optional<std::filesystem::path> current_loaf_path;
-bool show_browser = false;
-bool loaf_dirty = false;
-breadbin::core::LoafFile my_loaf;
-breadbin::gui::LoafBrowser browser(loaf_dirty, my_loaf);
+static std::optional<std::filesystem::path> current_loaf_path;
 
 static std::map<std::string, std::string> installed_apps;
 static std::vector<std::string> installed_app_names;
+static breadbin::core::LoafFile current_loaf;
 
 static std::filesystem::path loafs_dir() {
-    return std::filesystem::path(getenv("HOME")) / ".config" / "the-bread-bin" / "loafs";
+    return std::filesystem::path(std::getenv("HOME")) / ".config" / "the-bread-bin" / "loafs";
 }
 
-bool save_loaf_to_path(breadbin::core::LoafFile& loaf, const std::filesystem::path& path) {
+static bool save_loaf_to_path(breadbin::core::LoafFile& loaf, const std::filesystem::path & path) {
     if (loaf.save_to_file(path)) {
         loaf_dirty = false;
         return true;
     }
-    std::cerr << "Save failed! Cannot open file: " << path << std::endl;
     return false;
 }
 
-void save_loaf_as(breadbin::core::LoafFile& loaf, std::optional<std::filesystem::path>& path) {
+static void save_loaf_as(breadbin::core::LoafFile& loaf, std::optional<std::filesystem::path>& path) {
     std::filesystem::create_directories(loafs_dir());
 
     NFD::UniquePath outPath;
     nfdfilteritem_t filter[] = {{"Loaf files", "loaf"}};
 
     if (NFD::SaveDialog(outPath, filter, 1, loafs_dir().string().c_str()) == NFD_OKAY) {
+
         std::filesystem::path save_path = outPath.get();
         if (save_path.extension() != ".loaf") {
             save_path += ".loaf";
         }
+
         path = save_path;
         save_loaf_to_path(loaf, save_path);
     }
 }
 
+static void glfw_error_callback(int error, const char* description) {
+    std::fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
 std::map<std::string, std::string> get_installed_apps() {
     std::map<std::string, std::string> apps;
 
-    std::vector<std::filesystem::path> paths = {
-        "/usr/share/applications",
-        std::filesystem::path(getenv("HOME")) / ".local/share/applications"
-    };
+    std::vector<std::filesystem::path> paths = {"/usr/share/applications", std::filesystem::path(getenv("HOME")) / ".local/share/applications"};
 
     for (auto& dir : paths) {
         if (!std::filesystem::exists(dir)) {
@@ -109,48 +110,56 @@ void refresh_installed_apps() {
     }
 }
 
+
 int main() {
-    if (!glfwInit()) {
-        return -1;
-    }
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (!glfwInit())
+        return 1;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "The Bread Bin - Editor", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1400, 900, "The Bread Bin", nullptr, nullptr);
 
     if (!window) {
-        return -1;
+        return 1;
     }
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        return -1;
+    if (!gladLoadGL()) {
+        return 1;
     }
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
 
     ImGuiIO& io = ImGui::GetIO();
-    breadbin::theme::ApplyTheme();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    io.IniFilename = nullptr;
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
+    breadbin::theme::ApplyTheme();
+
     if (NFD::Init() != NFD_OKAY) {
-        std::cerr << "NFD init failed: " << NFD::GetError() << std::endl;
-        return -1;
+        return 1;
     }
 
     refresh_installed_apps();
 
-    breadbin::core::LoafFile current_loaf;
     current_loaf.name = "Untitled Loaf";
+
     static char name_buf[128];
     std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.name.c_str());
+
+    breadbin::gui::LoafBrowser loaf_browser(loaf_dirty, current_loaf);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -159,79 +168,102 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        if (ImGui::BeginMainMenuBar()) {
-            if (ImGui::BeginMenu("View")) {
-                if (ImGui::MenuItem("Open Loaf Browser", "Ctrl + B")) {
-                    show_browser = true;
+        const ImGuiViewport* vp = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(vp->Pos);
+        ImGui::SetNextWindowSize(vp->Size);
+        ImGui::SetNextWindowViewport(vp->ID);
+
+        ImGuiWindowFlags root_flags =ImGuiWindowFlags_NoDocking |
+            ImGuiWindowFlags_NoTitleBar |
+            ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoResize |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoBringToFrontOnFocus |
+            ImGuiWindowFlags_NoNavFocus |
+            ImGuiWindowFlags_MenuBar;
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+
+        ImGui::Begin("##BreadBinRoot", nullptr, root_flags);
+        ImGui::PopStyleVar(2);
+
+        ImGuiID dockspace_id = ImGui::GetID("BreadBinDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0, 0));
+
+        if (ImGui::BeginMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Load Loaf")) {
+                    std::filesystem::create_directories(loafs_dir());
+
+                    NFD::UniquePath inPath;
+                    nfdfilteritem_t filter[] = {{"Loaf files", "loaf"}};
+
+                    if (NFD::OpenDialog(inPath, filter, 1, loafs_dir().string().c_str()) == NFD_OKAY) {
+                        if (current_loaf.load_from_file(inPath.get())) {
+                            current_loaf_path = inPath.get();
+                            std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.name.c_str());
+                            loaf_dirty = false;
+                        }
+                    }
                 }
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Save", nullptr, false, loaf_dirty)) {
+                    if (current_loaf_path) {
+                        save_loaf_to_path(current_loaf, *current_loaf_path);
+                    }
+                    else {
+                        save_loaf_as(current_loaf, current_loaf_path);
+                    }
+                }
+
+                if (ImGui::MenuItem("Save As")) {
+                    save_loaf_as(current_loaf, current_loaf_path);
+                }
+
+                ImGui::Separator();
+                if (ImGui::MenuItem("Exit")) {
+                    glfwSetWindowShouldClose(window, true);
+                }
+
                 ImGui::EndMenu();
             }
-            ImGui::EndMainMenuBar();
-        }
 
-        if (ImGui::IsKeyDown(ImGuiMod_Ctrl) && ImGui::IsKeyPressed(ImGuiKey_B)) {
-            show_browser = !show_browser;
-        }
-
-        float menu_height = ImGui::GetFrameHeight();
-        float sidebar_width = 0.0f;
-
-        if (show_browser) {
-            sidebar_width = 300.0f;
-
-            ImGui::SetNextWindowPos(ImVec2(0, menu_height), ImGuiCond_FirstUseEver);
-            ImGui::SetNextWindowSize(ImVec2(sidebar_width, ImGui::GetIO().DisplaySize.y - menu_height), ImGuiCond_FirstUseEver);
-            ImVec2 pos = ImGui::GetWindowPos();
-            if (pos.x > 10.0f) {
-                sidebar_width = 0.0f;
+            if (ImGui::BeginMenu("View")) {
+                ImGui::MenuItem("Editor", nullptr, &show_editor);
+                ImGui::MenuItem("Loaf Browser", nullptr, &show_browser);
+                ImGui::EndMenu();
             }
 
-            browser.render(&show_browser);
+            ImGui::EndMenuBar();
         }
 
-        ImGui::SetNextWindowPos(ImVec2(sidebar_width, menu_height), ImGuiCond_Always);
-        ImGui::SetNextWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x - sidebar_width, ImGui::GetIO().DisplaySize.y - menu_height), ImGuiCond_Always);
+        ImGui::End();
 
-        ImGui::Begin("Bread Bin Editor", nullptr,
-                     ImGuiWindowFlags_NoDecoration |
-                     ImGuiWindowFlags_NoMove |
-                     ImGuiWindowFlags_NoResize |
-                     ImGuiWindowFlags_NoSavedSettings);
+        if (show_editor) {
+            ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
 
-        if (!loaf_dirty && std::string(name_buf) != current_loaf.name) {
-            std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.name.c_str());
-        }
+            ImGui::Begin("Bread Bin Editor", &show_editor);
 
-        if (ImGui::InputText("Loaf Name", name_buf, sizeof(name_buf))) {
-            current_loaf.name = name_buf;
-            loaf_dirty = true;
-        }
-
-        ImGui::SameLine();
-
-        if (ImGui::Button("Load Loaf")) {
-            std::filesystem::create_directories(loafs_dir());
-
-            NFD::UniquePath inPath;
-            nfdfilteritem_t filter[] = {{"Loaf files", "loaf"}};
-
-            if (NFD::OpenDialog(inPath, filter, 1, loafs_dir().string().c_str()) == NFD_OKAY) {
-                if (current_loaf.load_from_file(inPath.get())) {
-                    current_loaf_path = inPath.get();
-                    std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.name.c_str());
-                    loaf_dirty = false;
-                }
+            if (!loaf_dirty && std::string(name_buf) != current_loaf.name) {
+                std::snprintf(name_buf, sizeof(name_buf), "%s", current_loaf.name.c_str());
             }
-        }
 
-        ImGui::Separator();
+            if (ImGui::InputText("Loaf Name", name_buf, sizeof(name_buf))) {
+                current_loaf.name = name_buf;
+                loaf_dirty = true;
+            }
 
-        if (ImGui::Button("+ Add Action")) {
-            current_loaf.actions.push_back({breadbin::core::ActionType::App, "", {}});
-            loaf_dirty = true;
-        }
+            ImGui::Separator();
 
-        for (size_t i = 0; i < current_loaf.actions.size(); ++i) {
+            if (ImGui::Button("+ Add Action")) {
+                current_loaf.actions.push_back({breadbin::core::ActionType::App, "", {}});
+                loaf_dirty = true;
+            }
+
+            for (size_t i = 0; i < current_loaf.actions.size(); ++i) {
             auto& action = current_loaf.actions[i];
             ImGui::PushID(static_cast<int>(i));
 
@@ -240,8 +272,7 @@ int main() {
 
             std::string header = type_labels[type_index] + std::string(": ") + (action.target.empty() ? "<unset>" : action.target);
 
-            if (ImGui::CollapsingHeader(header.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
-            {
+            if (ImGui::CollapsingHeader(header.c_str(), ImGuiTreeNodeFlags_DefaultOpen)) {
                 if (ImGui::Combo("Type", &type_index, type_labels, IM_ARRAYSIZE(type_labels))) {
                     action.type = static_cast<breadbin::core::ActionType>(type_index);
                     loaf_dirty = true;
@@ -256,8 +287,7 @@ int main() {
                 }
 
                 if (ImGui::BeginCombo("Select App", current_label.c_str())) {
-                    for (auto& name : installed_app_names)
-                    {
+                    for (auto& name : installed_app_names) {
                         bool selected = installed_apps[name] == action.target;
 
                         if (ImGui::Selectable(name.c_str(), selected)) {
@@ -313,40 +343,30 @@ int main() {
             ImGui::PopID();
         }
 
-        ImGui::Separator();
-
-        ImGui::BeginDisabled(!loaf_dirty);
-        if (ImGui::Button("Save")) {
-            if (current_loaf_path) {
-                save_loaf_to_path(current_loaf, *current_loaf_path);
-            }
-            else {
-                save_loaf_as(current_loaf, current_loaf_path);
-            }
-        }
-        ImGui::EndDisabled();
-
-        ImGui::SameLine();
-        if (ImGui::Button("Save As")) {
-            save_loaf_as(current_loaf, current_loaf_path);
+            ImGui::End();
         }
 
-        ImGui::End();
+        if (show_browser) {
+            ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
+            loaf_browser.render(&show_browser);
+        }
 
         ImGui::Render();
-        int w,h;
+        int w, h;
         glfwGetFramebufferSize(window, &w, &h);
         glViewport(0, 0, w, h);
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
 
+    NFD::Quit();
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
-    NFD::Quit();
+
     glfwDestroyWindow(window);
     glfwTerminate();
 
